c1364ae13322ed5299fe710869b57f859c06e728
hisamparker <hisamparker@gmail.com>
2020-10-12 10:03:00 +0200

    spelling and link update

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>yy first React thingy!</h1>
+        <h1>my first React thingy!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           this is an experiment. it is maybe working?
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.reddit.com/r/reactjs/"
           target="_blank"
           rel="noopener noreferrer"
         >
