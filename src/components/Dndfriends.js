9ef7644fb2d47ae8719ef74417dac6c68a4c5df0
hisamparker <hisamparker@gmail.com>
2020-10-13 11:05:07 +0200

    learning jsx adding Dnd component

---
+++ src/components/Dndfriends.js
@@ -0,0 +1,14 @@
+import React from '.react':
+
+export default function Dndfriends() {
+    return (
+    <div>
+      <h2>Character name: {"Windex"}</h2>
+      <p>Weight: {150} kg</p>
+      <p>Awesome: {true}</p>
+      <p>Terrifying: {true}</p>
+      <p>What about null? {null}</p>
+      <p>And undefined? {undefined}</p>
+    </div>
+  );
+}
