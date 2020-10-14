37e7a0a2d8c95fc0fe24f2bbe81e104dda6d144d
hisamparker <hisamparker@gmail.com>
2020-10-14 10:37:47 +0200

    learning map jsx and map function for jsx

--- src/App.js
+++ src/App.js
@@ -4,16 +4,56 @@ import "./App.css";
 import Title from "./components/Title";
 import Dndfriends from "./components/Dndfriends";
 
+const dnd_characters = [
+  {
+    name: "Windex",
+    race: "Lizardfolk",
+    class: "druid",
+    weight: "150",
+    awesome: false,
+    terrifying: true,
+    abilities: ["Bite", "Heal", "Night vision", "Tough claws"]
+  },
+  {
+    name: "Bindex",
+    race: "Bizardfolk",
+    class: "Bruid",
+    weight: "250",
+    awesome: false,
+    terrifying: true,
+    abilities: ["Bite", "Beal", "Night vision", "Tough claws"]
+  },
+  {
+    name: "Cindex",
+    race: "Cizardfolk",
+    class: "Cruid",
+    weight: "350",
+    awesome: false,
+    terrifying: true,
+    abilities: ["Cite", "Heal", "Night vision", "Tough claws"]
+  },
+]
+
 function App() {
   return (
     <div className="App">
       <main>
         <Title content="DnD is noice" />
-        <Dndfriends />
+    
+        {dnd_characters.map(function (character){
+        return <Dndfriends 
+        name={character.name} 
+        race={character.race} 
+        class={character.class}
+        weight={character.weight}
+        awesome={character.awesome}
+        terrifying={character.awesome}
+        abilities={character.abilities} />;
+        })}
       </main>
 
       <header className="App-header">
-        <h1>my first React thingy is broken!</h1>
+        <h1>my first React thingy is no longer broken!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>this is an experiment. it is maybe working?</p>
         <a
--- src/components/Dndfriends.js
+++ src/components/Dndfriends.js
@@ -1,14 +1,21 @@
-import React from '.react':
+import React from "react";
 
-export default function Dndfriends() {
-    return (
+export default function Dndfriends(props) {
+  return (
     <div>
-      <h2>Character name: {"Windex"}</h2>
-      <p>Weight: {150} kg</p>
-      <p>Awesome: {true}</p>
-      <p>Terrifying: {true}</p>
-      <p>What about null? {null}</p>
-      <p>And undefined? {undefined}</p>
+      <h2>Character name: {props.name}</h2>
+      <p>Race: {props.race}</p>
+      <p>Class: {props.class}</p>
+      <p>Weight: {props.weight}</p>
+      <p>Awesome: {props.awesome ? "YES!" : "nope, not really"}</p>
+      <p>Terrifying: {props.terrifying ? "Very" : "nah, lovable"}</p>
+      <p>Abilities:
+      </p>
+      <ul>
+        {props.abilities.map(ability => {
+          return <li>{ability}</li>;
+        })}
+      </ul>
     </div>
   );
 }
